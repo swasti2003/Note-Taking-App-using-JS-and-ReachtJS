ee004205cf9c010a11f7a532543291bfa8cd3d82
swasti2003 <73665131+swasti2003@users.noreply.github.com>
2022-07-07 00:13:39 +0530

    Initial commit

---
+++ src/Components/Notes.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>This is Note 1</h1>
+      <p>
+        "The mind is not a book, to be opened at will and examined at leisure.
+        Thoughts are not etched on the inside of skulls, to be perused by an
+        invader. The mind is a complex and many-layered thing." — Severus Snape
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
